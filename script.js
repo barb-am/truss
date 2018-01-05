4d14b61c68c7bb5f44236829c64b828c89c2c02c
Jorge Silva <34944828+jorgesilva27@users.noreply.github.com>
2018-01-05 11:59:27 +0000

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by jorgesilva on 05/12/17.
- */
-$(window).load(function(){
-    $('.conto').find('img').each(function(){
-        var imgClass = (this.width/this.height > 1) ? 'wide' : 'tall';
-        $(this).addClass(imgClass);
-    })
-});
